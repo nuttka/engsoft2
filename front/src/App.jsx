92a4c1f93ee37309111a0941497f9ecb15e382d5
luissmonteiro <48304501+luissmonteiro@users.noreply.github.com>
2023-05-19 18:00:19 -0300

    adding first front files

---
+++ front/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+    return (
+        <h1>Teste</h1
+        
+    )
+}
+
+export default App;
