f5330882c0f4d50ad0d5ff21faef4e87c87a4410
Lucas August <nutkaugusto@gmail.com>
2023-05-19 14:51:13 -0300

    application changes and db initialize

--- realstate-api/src/main/java/com/engsoft2/realstate/RealstateApplication.java
+++ realstate-api/src/main/java/com/engsoft2/realstate/RealstateApplication.java
@@ -1,5 +1,8 @@
 package com.engsoft2.realstate;
 
+import com.engsoft2.realstate.services.DBService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,14 @@ public class RealstateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealstateApplication.class, args);
+	}
+
+	@Autowired
+	private DBService dbService;
+
+	@PostConstruct
+	private void init() {
+		dbService.instantiateTestDatabase();
 	}
 
 }
