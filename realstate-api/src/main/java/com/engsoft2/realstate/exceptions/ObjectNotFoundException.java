e88312188a624ce3f7ce067f7e9b327edd0b2e4e
Lucas August <nutkaugusto@gmail.com>
2023-05-21 10:29:58 -0300

    remoção de função não utilizada e adição da anotação @Serial

--- realstate-api/src/main/java/com/engsoft2/realstate/exceptions/ObjectNotFoundException.java
+++ realstate-api/src/main/java/com/engsoft2/realstate/exceptions/ObjectNotFoundException.java
@@ -1,14 +1,12 @@
 package com.engsoft2.realstate.exceptions;
 
-public class ObjectNotFoundException extends RuntimeException{
+import java.io.Serial;
 
+public class ObjectNotFoundException extends RuntimeException{
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ObjectNotFoundException(String msg) {
         super(msg);
-    }
-
-    public ObjectNotFoundException(String msg, Throwable cause) {
-        super(msg,cause);
     }
 }
