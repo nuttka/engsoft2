ae250d85c155d63cd322ec16f86c39af56f44e75
Lucas August <nutkaugusto@gmail.com>
2023-06-12 14:57:49 -0300

    teste github actions

---
+++ realstate-api/src/test/java/com/engsoft2/realstate/RealstateApplicationTest.java
@@ -0,0 +1,11 @@
+package com.engsoft2.realstate;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@EnableAutoConfiguration
+@ContextConfiguration(classes = RealstateApplicationTest.class)
+class RealstateApplicationTest {
+}
--- realstate-api/src/test/java/com/engsoft2/realstate/RealstateApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.engsoft2.realstate;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RealstateApplicationTests {
-}
