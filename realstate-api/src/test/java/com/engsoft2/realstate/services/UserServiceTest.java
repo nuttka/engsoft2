65e9f68cd769ce67503618060db20099d37b000f
Lucas August <nutkaugusto@gmail.com>
2023-06-17 20:17:09 -0300

    unit tests finished

--- realstate-api/src/test/java/com/engsoft2/realstate/services/UserServiceTest.java
+++ realstate-api/src/test/java/com/engsoft2/realstate/services/UserServiceTest.java
@@ -2,8 +2,10 @@ package com.engsoft2.realstate.services;
 
 import com.engsoft2.realstate.entities.User;
 import com.engsoft2.realstate.entities.dto.UserDTO;
+import com.engsoft2.realstate.exceptions.ObjectNotFoundException;
 import com.engsoft2.realstate.repositories.UserRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,6 +14,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
@@ -21,41 +25,97 @@ public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    private User existingUser;
+
+    @Before
+    public void setUp() {
+        existingUser = mockUser();
+    }
 
     @Test
-    public void testCreateNome() {
+    public void testCreate() {
         UserDTO userDTO = mockUserDTO();
+        User savedUser = mockUser();
 
-        Mockito.when(this.userRepository.save(Mockito.any())).thenReturn(mockUser());
-        User userReturn = this.userService.create(userDTO);
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(savedUser);
 
-        Assert.assertEquals(userDTO.getName(), userReturn.getName());
+        User createdUser = userService.create(userDTO);
+
+        Assert.assertEquals(userDTO.getName(), createdUser.getName());
+        Assert.assertEquals(userDTO.getEmail(), createdUser.getEmail());
+        Assert.assertEquals(userDTO.getPassword(), createdUser.getPassword());
     }
+
     @Test
-    public void testCreateEmail() {
-        UserDTO userDTO = mockUserDTO();
+    public void testFindById() {
+        int userId = 1;
+        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
 
-        Mockito.when(this.userRepository.save(Mockito.any())).thenReturn(mockUser());
-        User userReturn = this.userService.create(userDTO);
+        User foundUser = userService.findById(userId);
 
-        Assert.assertEquals(userDTO.getEmail(), userReturn.getEmail());
+        Assert.assertEquals(existingUser, foundUser);
     }
+
+    @Test(expected = ObjectNotFoundException.class)
+    public void testFindByIdInvalid() {
+        int userId = 1;
+        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        userService.findById(userId);
+    }
+
     @Test
-    public void testCreateSenha() {
+    public void testFindAll() {
+        List<User> userList = new ArrayList<>();
+        userList.add(existingUser);
+        Mockito.when(userRepository.findAll()).thenReturn(userList);
+        List<User> foundUsers = userService.findAll();
+
+        Assert.assertEquals(userList, foundUsers);
+    }
+
+    @Test
+    public void testDelete() {
+        int userId = 1;
+        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(existingUser));
+
+        userService.delete(userId);
+        Mockito.verify(userRepository, Mockito.times(1)).delete(existingUser);
+    }
+
+    @Test(expected = ObjectNotFoundException.class)
+    public void testDelete_InvalidId() {
+        int userId = 1;
+        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        userService.delete(userId);
+    }
+
+    @Test
+    public void testUpdate() {
         UserDTO userDTO = mockUserDTO();
+        User updatedUser = mockUser();
+        Mockito.when(userRepository.findById(userDTO.getId())).thenReturn(Optional.of(existingUser));
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(updatedUser);
 
-        Mockito.when(this.userRepository.save(Mockito.any())).thenReturn(mockUser());
-        User userReturn = this.userService.create(userDTO);
+        User result = userService.update(userDTO);
 
-        Assert.assertEquals(userDTO.getPassword(), userReturn.getPassword());
+        Assert.assertEquals(updatedUser, result);
     }
 
+    @Test(expected = ObjectNotFoundException.class)
+    public void testUpdateInvalid() {
+        UserDTO userDTO = mockUserDTO();
+        Mockito.when(userRepository.findById(userDTO.getId())).thenReturn(Optional.empty());
 
-    public UserDTO mockUserDTO() {
-        return new UserDTO(0, "Test", "teste@teste.com", "senha");
+        userService.update(userDTO);
     }
 
-    public User mockUser() {
-        return new User(0, "Test", "teste@teste.com", "senha", new ArrayList<>());
+    private UserDTO mockUserDTO() {
+        return new UserDTO(1, "Test", "test@test.com", "password");
+    }
+
+    private User mockUser() {
+        return new User(1, "Test", "test@test.com", "password", new ArrayList<>());
     }
 }
